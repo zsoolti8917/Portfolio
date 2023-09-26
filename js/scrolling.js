bc6b2e24fec754d7657d023b5c5ec1a948e957c1
zsoolti8917 <zsolt.varju.rl@gmail.com>
2023-09-27 01:36:58 +0200

    Added links

--- js/scrolling.js
+++ js/scrolling.js
@@ -6,3 +6,44 @@ $(document).ready(function() {
     });
   });
   
+const form = document.querySelector('.contact-form')
+const userEmail = document.getElementById('email');
+const submit = document.querySelector('.submit-button')
+const userName = document.getElementById('name')
+const emailError = document.querySelector('.email-error')
+
+
+
+userEmail.addEventListener('input', (event)=>{
+  if (userEmail.validity.valid) {
+    emailError.textContent = ""; 
+  } else {
+    errorMessage();
+  }
+})
+
+
+
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    if (!userEmail.validity.valid) {
+    errorMessage()
+    } else {
+        Notiflix.Notify.success('Thank you for reaching out to us!')
+        form.reset()
+    }
+}
+   
+)
+
+function errorMessage(){
+    if (userEmail.validity.valueMissing) {
+        emailError.textContent ="You need to enter an email address.";
+      } else if (userEmail.validity.typeMismatch) {
+        emailError.textContent ="Entered value needs to be an email address.";
+      } else if (userEmail.validity.tooShort) {
+        emailError.textContent = `Email should be at least ${userEmail.minLength} characters; you entered ${userEmail.value.length}.`; ;
+      }
+    
+}
